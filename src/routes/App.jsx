f77f0c0ba5809454e966490e39a0ee0244d90e2c
ShodaDev0081 <paul.w.schacher@gmail.com>
2022-09-20 08:57:53 +0200

    changes app.jsx + app.css

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,23 +20,35 @@ const App = () => {
       <div>
         <section className="section section-1">
           <h2 className="heading color-white"> About</h2>
-          <p>Hello! I'm ShodaDev. A young full-stack developer that loves to code in Javascript. I also familar with HTML, CSS and TS. I currently learning React.</p>
+          <p>
+            Hello! I'm ShodaDev. A young full-stack developer that loves to code in Javascript. I also familar with HTML, CSS and TS. I currently learning React. <br/>
+            I currently working at <a href="https://alpha-host.de">Alpha-Host.de</a> as Supporter.
+          </p>
         </section>
         <section className="section section-2">
           <h2 className="heading color-white"> Experience</h2>
-          <p>I code for 2 years now. I started with Discord Bots (discord.js) and then learned to make APIs with Express.</p>
+          <p>
+            I code for 2 years now. I started with Discord Bots (discord.js) and then learned to make APIs with Express. <br/>
+            Now I do web-development with some JS-Web-Frameworks like React or Fresh.
+          </p>
         </section>
         <section className="section section-3">
           <h2 className="heading color-white"> Projects</h2>
-          <p>I'm currently developing a project with React, Express and Supabase! Its gonna be a clone of <a href="https://uptimerobot.com">UptimeRobot</a>.</p>
+          <p>I'm currently developing a big project with React, Express and Supabase! Its gonna be a clone of <a href="https://uptimerobot.com">UptimeRobot</a>. <br/> <br/>
+            I also have some little side-projects like <br/> 
+            <a href="https://github.com/">my Portfolio (React + Vite)</a>, <br/> 
+            <a href="https://github.com/">a Imagesearch API + Wrapper (Typescript)</a> or <br/>
+            <a href="https://github.com/">a Linux-Install script (Shell)</a>. <br/>
+            You can find them on my <a href="https://github.com/ShodaDev0081">Github</a>.  
+          </p>
         </section>
         <section className="section section-4">
           <h2 className="heading color-white"> Skills</h2>
-          <p>HTML<br/>CSS<br/>JS<br/>TS<br/>Node.js<br/>React<br/>Express<br/>Discord.js<br/>Bootstrap<br/>TailwindCSS<br/>Supabase<br/>SSH<br/>SFTP<br/>Shell/Bash<br/>Pterodactyl</p>
+          <p>HTML<br/>CSS<br/>JS<br/>TS<br/>Node.js<br/>React<br/>Express<br/>Discord.js<br/>Deno<br/>Fresh<br/>Bootstrap<br/>TailwindCSS<br/>Supabase<br/>SSH<br/>SFTP<br/>Shell/Bash<br/>Pterodactyl</p>
         </section>
         <section className="section section-5">
           <h2 className="heading color-white"> Contact</h2>
-          <p>Discord: <a href="https://discord.com/users/898285494373347379" target="_blank">ShodaDev#0081</a>  <br/> Github: <a href="https://github.com/ShodaDev0081" target="_blank">ShodaDev0081</a> <br/> Website: <a href="https://shoda-uwu.eu">https://shoda-uwu.eu/</a> <br/> E-Mail: <a href="mailto:hello@shoda-uwu.eu">hello@shoda-uwu.eu</a></p>
+          <p>Discord: <a hre="fhttps://discord.com/users/898285494373347379" target="_blank">ShodaDev#0081</a>  <br/> Github: <a href="https://github.com/ShodaDev0081" target="_blank">ShodaDev0081</a> <br/> Website: <a href="https://shoda-uwu.eu">https://shoda-uwu.eu/</a> <br/> E-Mail: <a href="mailto:hello@shoda-uwu.eu">hello@shoda-uwu.eu</a></p>
         </section>
         </div>
         <div style={{paddingTop: 20}}>
